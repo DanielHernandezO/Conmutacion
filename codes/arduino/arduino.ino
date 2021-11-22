a1020638cadb0fbc3cb5e73eaa71bd78395453fd
MrSossa <83780739+MrSossa@users.noreply.github.com>
2021-11-21 20:03:07 -0500

    Add files via upload

--- codes/arduino/arduino.ino
+++ codes/arduino/arduino.ino
@@ -1,16 +1,16 @@
 #include <Servo.h>
-#include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
-
 
 #define red A0
 #define blue A1
 #define green A2
 #define INB 23
 #define INA 22
-#define DHT11pin A3
 #define nodeCom A4
+#define buzzer 2
+#define smoke A3
+int sensorThreshold = 400;
 const byte numRows = 4;
 const byte numCols = 4;
 
@@ -21,14 +21,12 @@ char keymap[numRows][numCols]={
   {'*','0','#','D'},
 };
 
-byte rowPins[numRows] = {25,27,29,31};
-byte colsPins[numRows] = {33,35,37,39};
+byte rowPins[numRows] = {33,35,37,39};
+byte colsPins[numRows] = {25,27,29,31};
 
 Keypad teclado = Keypad(makeKeymap(keymap),rowPins,colsPins, numRows, numCols);
 LiquidCrystal_I2C lcd(0x27,16,2); //(0x3f,16,2) || (0x27,16,2)  ||(0x20,16,2)
-
 Servo servoMotor;
-DHT dht(DHT11pin,DHT11);
 char tecla;
 String contrasenia="";
 void setup() {
@@ -40,7 +38,10 @@ void setup() {
   pinMode(green,OUTPUT);
   pinMode(INB,OUTPUT);
   pinMode(INA,OUTPUT);
-  dht.begin();
+  pinMode(nodeCom,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(smoke, INPUT);
+  
   lcd.init();
   lcd.backlight();
   lcd.clear();
@@ -49,6 +50,12 @@ void setup() {
   delay(1000);
   lcd.clear();
   lcd.noBacklight();
+  digitalWrite(red,0);
+  digitalWrite(blue,0);
+  digitalWrite(green,0);
+  digitalWrite(INB,0);
+  digitalWrite(INA,0);
+  servoMotor.write(0);
 }
 //servoDATA A0
 void leds(int r,int b, int g){
@@ -59,9 +66,13 @@ void leds(int r,int b, int g){
 
 void loop() {
   char aux;
-  
+  //Serial.println(analogRead(smoke));
+  if (analogRead(smoke) > sensorThreshold){
+    tone(buzzer, 1000, 200);
+  }
   if (Serial1.available()){
     aux = Serial1.read();
+    //Serial.println(aux);
     //puerta
     if(aux=='p'){
       while (!Serial1.available());
@@ -88,6 +99,7 @@ void loop() {
     }else if(aux=='v'){
       while (!Serial1.available());
       aux = Serial1.read();
+      Serial.println(aux);
       if (aux=='1'){
         digitalWrite(INA,0);
         digitalWrite(INB,1);
@@ -96,7 +108,7 @@ void loop() {
         digitalWrite(INB,0);
       }
     }else if(aux == 'd'){
-      Serial1.print("0000");
+      //falta
     }else if(aux == 'c'){
       contrasenia="";
       for (int i = 0; i < 4; i++){
@@ -124,12 +136,17 @@ void loop() {
     }
     lcd.clear();
     lcd.noBacklight();
-    Serial.println("c: "+contrasenia);
-    if (tecla=='D' && pass == contrasenia){
-      servoMotor.write(90);
-      delay(2000);
-      servoMotor.write(0);
-      delay(1000);
+    if (tecla=='D'){
+      //Serial.println("c: "+pass);
+      if (pass == contrasenia){
+        servoMotor.write(90);
+        delay(2000);
+        servoMotor.write(0);
+        delay(1000);
+      }else{
+        Serial.println("Error");
+        tone(buzzer, 2000,3000);
+      }
     }else{
       Serial.println("Canceled");
     }
--- codes/node/node.ino
+++ codes/node/node.ino
@@ -1,18 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseESP8266.h>
 
+//char *ssid  = "IoT-B19";
+//char *password = "lcontrol2020*";
 
-char *ssid  = "IoT-B19";
-char *password = "lcontrol2020*";
+char *ssid  = "SOSA PENA";
+char *password = "1BX27e8JDU1";
 
 char *host = "https://venom-293e9-default-rtdb.firebaseio.com/";
 char *auth = "QfdBEvwB5uBhBx75W4XqAhscLSEunhC3GxzwXvGU";
-
+String p,l,v,d,c;
 FirebaseData firebasedata;
 void setup() {
-  pinMode(A0,INPUT);
   
   Serial.begin(115200);
+  Serial1.begin(115200);
   Serial.println();
 
   WiFi.begin(ssid, password);
@@ -26,6 +28,11 @@ void setup() {
   Serial.println(WiFi.localIP());
   Firebase.begin(host,auth);
   delay(2000);
+  p = "0";
+  l = "0";
+  v = "0";
+  d = "0";
+  c = "0000";
 }
 //abrir puerta (voz) -> p1 -> p0
 //caja fuerte(teclado matricial)
@@ -35,26 +42,29 @@ void setup() {
 //ventilador -> v0 -> v1
 void loop() {
   Firebase.getString(firebasedata,"commands/P/Estado");
-  Serial.print("p"+firebasedata.stringData());
-  Firebase.getString(firebasedata,"commands/l/Estado");
-  Serial.print("l"+firebasedata.stringData());
-  Firebase.getString(firebasedata,"commands/v/Estado");
-  Serial.print("v"+firebasedata.stringData());
-  
-  //Firebase.getString(firebasedata,"commands/D/Estado");
-  Serial.print("d");
-  /*String contrasenia="";
-  char aux;
-  for (int i = 0; i < 4; i++){
-    while (!Serial1.available());
-    aux = Serial1.read();
-    contrasenia += aux;
+  if (firebasedata.stringData()!= p){
+    Serial.print("p"+firebasedata.stringData());
+    p = firebasedata.stringData();
   }
-  Firebase.setString(firebasedata,"commands/Contrasenia",contrasenia);
-  */N
-  Firebase.getString(firebasedata,"commands/Contrasenia");
-  Serial.print("c"+firebasedata.stringData());
+  Firebase.getString(firebasedata,"commands/l/Estado");
+  if (firebasedata.stringData()!= l){
+    Serial.print("l"+firebasedata.stringData());
+    l = firebasedata.stringData();
+  }
+  Firebase.getString(firebasedata,"commands/v/Estado");
+  if (firebasedata.stringData()!= v){
+    Serial.print("v"+firebasedata.stringData());
+    v = firebasedata.stringData();
+  }
   
+  //Firebase.setDouble(firebasedata,"commands/D/Humedad",humi);
+  
+  Firebase.getString(firebasedata,"commands/Contrasenia");
+  if (firebasedata.stringData()!= c){
+    Serial.print("c"+firebasedata.stringData());
+    c = firebasedata.stringData();
+  }
+
   delay(1000);
   //Serial.print("v0");
 }
